906d6f2919688e3627e84780a1cc6edb42b041e5
basiibas <bas.riemprasert@gmail.com>
2018-09-04 15:30:02 +0200

    Fjerne til startsversjon

--- app/src/main/java/com/example/bas/livssyklus/Hoved.java
+++ app/src/main/java/com/example/bas/livssyklus/Hoved.java
@@ -16,33 +16,6 @@ public class Hoved extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoved);
 
-        Toolbar myToolbar = (Toolbar)findViewById(R.id.myToolbar);
-        setActionBar(myToolbar);
-
-
-
-
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_hoved, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
-            case R.id.aktivitet2:
-                break;
-
-
-
-                default:
-                    return super.onOptionsItemSelected(item);
-        }return true;
-    }
-
 
 }
