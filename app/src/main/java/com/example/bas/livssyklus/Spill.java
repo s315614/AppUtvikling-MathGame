5a910ae3420be68d6080ae691be243a4c49acd27
s315614 <s315614@stud.hioa.no>
2018-09-13 13:47:59 +0200

    backspace

--- app/src/main/java/com/example/bas/livssyklus/Spill.java
+++ app/src/main/java/com/example/bas/livssyklus/Spill.java
@@ -1,22 +1,18 @@
 package com.example.bas.livssyklus;
 
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Random;
-import java.util.Set;
 
 public class Spill extends AppCompatActivity {
 
@@ -195,6 +191,24 @@ public class Spill extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onBackPressed() {
+        //Toast.makeText(getApplication(),"Hello",Toast.LENGTH_LONG).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Exit");
+        builder.setMessage("Vil du gå ut av spillet?");
+
+
+        builder.setPositiveButton("Avslutt", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent goToMain = new Intent(getApplicationContext(), Hoved.class);
+                startActivity(goToMain);
+                finish();
+            }
+        });
 
     public void lagreDataene(int riktig, int galt){
         SharedPreferences prefs = getApplication().getSharedPreferences("Resultat",MODE_PRIVATE);
