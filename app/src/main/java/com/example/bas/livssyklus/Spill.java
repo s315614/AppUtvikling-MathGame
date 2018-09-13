6180632b0dfc7ec0ef2290415730d24bcdb9aa73
s315614 <s315614@stud.hioa.no>
2018-09-13 14:18:23 +0200

    ..

--- app/src/main/java/com/example/bas/livssyklus/Spill.java
+++ app/src/main/java/com/example/bas/livssyklus/Spill.java
@@ -1,11 +1,11 @@
 package com.example.bas.livssyklus;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -196,7 +196,7 @@ public class Spill extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         //Toast.makeText(getApplication(),"Hello",Toast.LENGTH_LONG).show();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setTitle("Exit");
         builder.setMessage("Vil du gå ut av spillet?");
@@ -209,7 +209,15 @@ public class Spill extends AppCompatActivity {
                 startActivity(goToMain);
                 finish();
             }
+
         });
+        builder.setNegativeButton("Avbrytt", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog dialog = builder.show();
     }
 
     public void lagreDataene(int riktig, int galt){
